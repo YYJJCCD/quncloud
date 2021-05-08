a5db39c788a4e81160cd88c7fc991cc567d1b4bf
unknown <2396069874@qq.com>
2021-05-08 23:54:04 +0800

    fix: bug修复

--- quncolud.py
+++ quncolud.py
@@ -3,28 +3,31 @@ import numpy as np
 from worddb import wordDB
 import jieba
 import arrow
+import os
 from wordcloud import WordCloud, STOPWORDS
+
 
 class qunCloud:
     def __init__(self, qunid):
         self.qunid = qunid
         db = wordDB()
         self.data = db.select(self.qunid)
-    
+
     def getData(self, msgText):
         info = msgText.split()
         op = info[1]
         args = list(map(lambda x: int(x), info[2:]))
+        if op == '精确':
+            return self.selectDate(*args)
+        args.append(0)
         argDict = {
-            '年份': (args[0], 0),
+            '年份': ((args[0],), 0),
             '月份': ((0, args[0]), 1),
             '日期': ((0, 0, args[0]), 2),
         }
-        if op == '精确':
-            return self.selectDate(*args)
         args = argDict[op]
-        return self.selectLastTime(*(args[0]), timeType=args[1])
-
+        data = self.selectLastTime(*(args[0]), timeType=args[1])
+        return data
 
     def selectDate(self, year=-1, month=-1, day=-1):
         timeTuple = year, month, day
@@ -40,25 +43,34 @@ class qunCloud:
         data = map(lambda x: x[0], data)
         return ' '.join(data)
 
-    def addStopWords(self, words):
-        open('./res/%suserstopwords.txt' % self.qunid, 'a', encoding='utf8').write('\n'.join(words))
+    def addQunStopWords(self, words):
+        with open('./res/%s_stopwords.txt' % self.qunid, 'a', encoding='utf8') as f:
+            f.write('\n'.join(words))
+
+    def addGlobalStopWords(self, words):
+        with open('./res/stopwords.txt' % self.qunid, 'a', encoding='utf8') as f:
+            f.write('\n'.join(words))
 
     def selectLastTime(self, yearCnt=0, monthCnt=0, dayCnt=0, timeType=0):
         now = arrow.now()
         lastTime = now.shift(years=-yearCnt, months=-monthCnt, days=-dayCnt)
         timeTuple = lastTime.year, lastTime.month, lastTime.day
         data = filter(lambda x: x[1][timeType] == timeTuple[timeType], self.data)
-        data = map(lambda x: x[0], data)
-        return ' '.join(data)
+        data = list(map(lambda x: x[0], data))
+        data = ' '.join(data)
+        return data
 
     def solve(self, data):
+        print('solve', data)
         default_mode = jieba.cut(data, cut_all=False)
         default_mode = list(filter(lambda x: len(x) > 1, default_mode))
         text = ' '.join(default_mode)
         alice_mask = np.array(Image.open('./res/a.png'))
         cnStopWords = set(map(str.strip, open('./res/stopwords.txt', encoding='utf8').readlines()))
-        userStopWords = set(map(str.strip, open('./res/%s_userstopwords.txt' % self.qunid , encoding='utf8').readlines()))
-        stopwords = STOPWORDS|cnStopWords|userStopWords
+        if not os.path.exists('./res/%s_stopwords.txt' % self.qunid):
+            open('./res/%s_stopwords.txt' % self.qunid, 'a').close()
+        userStopWords = set(map(str.strip, open('./res/%s_stopwords.txt' % self.qunid, encoding='utf8').readlines()))
+        stopwords = STOPWORDS | cnStopWords | userStopWords
         wc = WordCloud(
             font_path=r'./font/simhei.ttf',
             background_color="white",
